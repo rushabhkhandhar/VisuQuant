ff836cc70530cd53518b7273321fad1def5dfe18
Rushabh Khandhar <rushabhkhandhar@example.com>
2026-08-15 01:09:47 +0530

    Updated till best Relative Strength of cagr 21%

--- Quant_backend/src/screener/pipeline/compare_strategies.py
+++ Quant_backend/src/screener/pipeline/compare_strategies.py
@@ -27,9 +27,9 @@ FRICTION_PCT = 0.0015  # 0.15% cost per trade leg
 
 STRATEGIES = [
     # {"name": "Trend Pullback", "func": trend_pullback_eval, "risk_atr": 1.5, "reward_atr": 3.0},
-    # {"name": "Momentum Breakout", "func": momentum_breakout_eval, "risk_atr": 3.0, "reward_atr": 6.0},
-    # {"name": "Oversold Uptrend", "func": oversold_uptrend_eval, "risk_atr": 2.5, "reward_atr": 5.0},
-    # {"name": "Volatility Compression", "func": volatility_compression_eval, "risk_atr": 2.5, "reward_atr": 5.0},
+    # {"name": "Momentum Breakout", "func": momentum_breakout_eval, "risk_atr": 2.0, "reward_atr": 4.0},
+    # {"name": "Oversold Uptrend", "func": oversold_uptrend_eval, "risk_atr": 2.0, "reward_atr": 4.0},
+    # {"name": "Volatility Compression", "func": volatility_compression_eval, "risk_atr": 1.5, "reward_atr": 3.0},
     {"name": "Relative Strength", "func": relative_strength_eval, "risk_atr": 2.0, "reward_atr": 4.0},
 ]
 
